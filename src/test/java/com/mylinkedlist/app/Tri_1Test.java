01bb4c18adfb5c8709a681291c3e11883cf3f2b1
vamshikrishna5944 <vamshikrishna.guggulla@iiitb.ac.in>
2023-11-27 19:02:53 +0530

    hello

---
+++ src/test/java/com/mylinkedlist/app/Tri_1Test.java
@@ -0,0 +1,59 @@
+package com.mylinkedlist.app;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Tri_1Test {
+
+    @Test
+    public void testFindMaximumXOR() {
+        tri_1 tri = new tri_1();
+
+        // Test Case 1
+        int[] nums1 = {3, 10, 5, 25, 2, 8};
+        assertEquals(28, tri.findMaximumXOR(nums1));
+
+
+        // Test Case 3
+        int[] nums3 = {7};
+        assertEquals(0, tri.findMaximumXOR(nums3));
+
+        // Test Case 4
+        int[] nums4 = {0, 0, 0, 0};
+        assertEquals(0, tri.findMaximumXOR(nums4));
+
+        // Test Case 5
+        int[] nums5 = {15, 15, 15, 15};
+        assertEquals(0, tri.findMaximumXOR(nums5));
+
+        // Test Case 6
+        int[] nums6 = {-2, -5, -8, -1};
+        assertEquals(7, tri.findMaximumXOR(nums6));
+
+        // Test Case 7
+        int[] nums7 = {2, -5, 8, -1, 3};
+        assertEquals(-3, tri.findMaximumXOR(nums7));
+
+        // Test Case 8
+        int[] nums8 = {1000000, 999999, 888888, 777777};
+        assertEquals(413193, tri.findMaximumXOR(nums8));
+
+        // Test Case 9
+        int[] nums9 = {3, 5, 3, 5, 10};
+        assertEquals(15, tri.findMaximumXOR(nums9));
+
+        // Test Case 10 (Performance Test)
+        int[] nums10 = generateLargeArray();
+        long startTime = System.currentTimeMillis();
+        tri.findMaximumXOR(nums10);
+        long endTime = System.currentTimeMillis();
+    }
+
+    private int[] generateLargeArray() {
+        // Generate a large array of random integers
+        int[] nums = new int[1000000];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (int) (Math.random() * Integer.MAX_VALUE);
+        }
+        return nums;
+    }
+}
---
+++ src/test/java/com/mylinkedlist/app/tri_2Test.java
@@ -0,0 +1,36 @@
+package com.mylinkedlist.app;
+import java.util.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class tri_2Test {
+
+    @Test
+    public void maximizeXor() {
+        tri_2 solution = new tri_2();
+
+        // Test case with a single element in nums and a single query
+        int[] nums1 = {5};
+        int[][] queries1 = {{2, 3}};
+        assertArrayEquals(new int[]{-1}, solution.maximizeXor(nums1, queries1));
+
+        // Test case with multiple elements in nums and multiple queries
+        int[] nums2 = {3, 10, 5, 25, 2, 8};
+        int[][] queries2 = {{3, 7}, {14, 15}, {0, 10}};
+        assertArrayEquals(new int[]{6, 13, 10}, solution.maximizeXor(nums2, queries2));
+
+        // Test case with nums and queries containing negative numbers
+        int[] nums3 = {-5, -2, -8, -1};
+        int[][] queries3 = {{-10, -3}, {-6, -5}};
+        assertArrayEquals(new int[]{14, 2}, solution.maximizeXor(nums3, queries3));
+
+        // Test case with nums and queries containing large numbers
+        int[] nums4 = {1000000, 500000, 750000};
+        int[][] queries4 = {{800000, 900000}, {700000, 950000}};
+        assertArrayEquals(new int[]{758816,855872}, solution.maximizeXor(nums4, queries4));
+
+        // Test case with an empty nums array and queries
+        int[] nums5 = {};
+        int[][] queries5 = {{1, 5}, {0, 3}};
+        assertArrayEquals(new int[]{-1, -1}, solution.maximizeXor(nums5, queries5));
+    }
+}
